67e264a4126ed864ee7b648d965c780e077cfb01
rameez <rameez012001@gmail.com>
2023-06-17 16:31:37 +0530

    first-page2

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,13 @@
             $(document).on('click', '.menu-icon', function() {
                 $(this).toggleClass('active');
                 $('.left-sidebar').toggleClass('active')
+                $('.about-text').toggleClass('active')
+                $('#logo').toggleClass('inactive')
+                $('.item-wrapper').toggleClass('inactive')
+                $('footer').toggleClass('inactive')
             });
+            
         });
     }
 })(jQuery);
+AOS.init();
