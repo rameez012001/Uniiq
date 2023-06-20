682ab4517276ac920ab12183fa13ca5cfb5b0cd0
rameez <rameez012001@gmail.com>
2023-06-20 17:33:47 +0530

    blog-single-update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,21 @@
                 $('.item-wrapper').toggleClass('inactive')
                 $('footer').toggleClass('inactive')
             });
-            
         });
     }
 })(jQuery);
+$(document).ready(()=>{
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        dots:true,
+        nav:true,
+        items:3,
+        margin:94,
+        autoWidth:true,
+        center:true,
+        smartSpeed:1500,
+        autoplay:true,
+        autoplayTimeout:3000,
+    });
+})
 AOS.init();
