8061be70dd79eb53f1ae73c8af821995aa12a61e
rameez <rameez012001@gmail.com>
2023-07-01 04:25:01 +0530

    update cleanCode1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 (function($) {
     "use strict";
     menuControl();
+    skillFill();
+    
+
     function menuControl() {
         $(document).ready(function() {
             $(document).on('click', '.menu-icon', function() {
@@ -12,6 +15,18 @@
                 $('footer').toggleClass('inactive')
             });
         });
+    }
+    function skillFill() {
+        if ($('.skill-fill')[0]) {
+            $(".skill-fill:not(.animation-done").each(function (i) {
+                var top_of_object = $(this).offset().top;
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                // if ((bottom_of_window - 70) > top_of_object) {
+                    $(this).width($(this).data("fill"));
+                    // $(this).addClass('animation-done');
+                // }
+            });
+        }
     }
 })(jQuery);
 $(document).ready(()=>{
@@ -26,10 +41,11 @@ $(document).ready(()=>{
         smartSpeed:1500,
         autoplay:true,
         autoplayTimeout:3000,
+        autoplayHoverPause: true
     });
 })
 $(document).mousemove(function(e) {
     var roundCursor = $('.cursor');
     roundCursor.css({ top: e.clientY, left: e.clientX });
   });
-AOS.init();
+  AOS.init();
