c8bb37f2d74f0c3fb8c01e8a4755817a0603d27d
rameez <rameez012001@gmail.com>
2023-07-06 19:51:24 +0530

    bwp color update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
                 $('#logo').toggleClass('inactive');
                 $('.item-wrapper').toggleClass('inactive');
                 $('footer').toggleClass('inactive');
+                type();
             });
         }
         function skillFill() {
@@ -70,3 +71,66 @@ $(document).ready(function() {
 });
 $(document).ready(()=>{AOS.init();})
 $(document).on('scroll',()=>{AOS.refresh();})
+function type(){   
+  var words = [
+      `My name is Jason Williams and I am a photographer senior graphic designer from New York.`
+  ];
+
+  var delay = 2000; // Delay before typing each word (in milliseconds)
+  var typingSpeed = 50; // Speed of typing animation (in milliseconds)
+
+  var typingText = document.getElementById("typing-text");
+
+  function typeWords(index) {
+      if (index < words.length) {
+          var word = words[index];
+          var typedWord = "";
+          var isDeleting = false;
+
+          var interval = setInterval(function() {
+              if (isDeleting) {
+                  typedWord = word.substr(0, typedWord.length - 1);
+              } else {
+                  typedWord = word.substr(0, typedWord.length + 1);
+              }
+
+              typingText.innerHTML = typedWord;
+              // typingText.innerHTML = typedWord.replace(/(^[^,]+)/, '<span style="color: red">$1</span>').replace(/(,)/, '<span style="color: red">$1</span>');
+
+
+
+              if (!isDeleting && typedWord === word) {
+                  isDeleting = true;
+                  clearInterval(interval);
+                  setTimeout(function() {
+                      typeWords(index);
+                  }, delay);
+              } else if (isDeleting && typedWord === "") {
+                  isDeleting = false;
+                  clearInterval(interval);
+                  setTimeout(function() {
+                      typeWords(index + 1);
+                  }, delay);
+              }
+          }, typingSpeed);
+      }
+  }
+
+  typeWords(0);
+}
+
+function mousemove() {
+  var x = document.querySelectorAll('#home .item-wrapper .items .image');
+  $(x).mouseenter(() => {
+    $('.cursor').addClass('onimg');
+    $('.cursor').addClass('vector');
+    $('.dot').addClass('inactive');
+  }).mouseleave(() => {
+    $('.cursor').removeClass('onimg');
+    $('.cursor').removeClass('vector');
+    $('.dot').removeClass('inactive');
+  });
+}
+
+mousemove();
+
